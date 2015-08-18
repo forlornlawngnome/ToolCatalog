482dd4a0e112e26a0bf68c4901929b5c568888db
Megan Wenzinger <forlornlawngnome@gmail.com>
2015-08-18 17:25:50 -0400

    bad query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :authorize, :only=>:index
   def index
     @hours = Hour.order(:day_of_week).group_by{|h| h.day_of_week}
-    @courses_approval = CoursesPerson.where(:approved=>false, :approved=>nil).group_by{|a| a.course.name}
+    @courses_approval = CoursesPerson.where(:approved=>false).group_by{|a| a.course.name}
     @tools_maintenance = Tool.needs_action
   end
 end
